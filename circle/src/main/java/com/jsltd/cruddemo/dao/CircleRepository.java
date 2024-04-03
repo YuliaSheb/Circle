6d32dee9f0a67060c9ae89ed1ebe3c1ca7d44567
Yulia <79199794+YuliaSheb@users.noreply.github.com>
2024-04-03 11:28:57 +0300

    Add files via upload

--- circle/src/main/java/com/jsltd/cruddemo/dao/CircleRepository.java
+++ circle/src/main/java/com/jsltd/cruddemo/dao/CircleRepository.java
@@ -1,17 +1,21 @@
 package com.jsltd.cruddemo.dao;
 
 import com.jsltd.cruddemo.entity.Circle;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+
 
 @Repository
 public class CircleRepository {
@@ -76,6 +80,70 @@ public class CircleRepository {
         SqlParameterSource parameters = new MapSqlParameterSource("id", id);
 
         return jdbcTemplate.query(sql, parameters, new CircleMapper());
+    }
+
+    JedisPool pool = new JedisPool("localhost", 6379);
+    public List<Circle> getCashedCircle(int id){
+        String sql = "SELECT " +
+                "circles.id AS id, " +
+                "circles.name AS name, " +
+                "circles.parent_id AS parentId, " +
+                "circles.type AS type, " +
+                "(SELECT " +
+                "  JSON_AGG( " +
+                "    JSON_BUILD_OBJECT( " +
+                "      'id', circle_users.id, " +
+                "      'role', circle_users.circle_role, " +
+                "      'capacity', circle_users.capacity, " +
+                "      'employee_id', circle_users.employee_id " +
+                "    ) " +
+                "  ) " +
+                "FROM " +
+                "  circle_users " +
+                "WHERE " +
+                "  circle_users.circle_id = circles.id " +
+                ") AS users, " +
+                "(SELECT " +
+                "  JSON_AGG( " +
+                "    JSON_BUILD_OBJECT( " +
+                "      'id', circle_child.id, " +
+                "      'name', circle_child.name, " +
+                "      'parentId', circle_child.parent_id, " +
+                "      'type', circle_child.type, " +
+                "      'users', ( " +
+                "        SELECT " +
+                "          JSON_AGG( " +
+                "            JSON_BUILD_OBJECT( " +
+                "              'id', circle_child_users.id, " +
+                "              'role', circle_child_users.circle_role, " +
+                "              'capacity', circle_child_users.capacity, " +
+                "              'circle_id', circle_child_users.employee_id " +
+                "            ) " +
+                "        ) " +
+                "        FROM " +
+                "          circle_users AS circle_child_users " +
+                "        WHERE " +
+                "          circle_child_users.circle_id = circle_child.id " +
+                "      ) " +
+                "      ) " +
+                "    ) " +
+                "FROM " +
+                "  circles AS circle_child " +
+                "WHERE " +
+                "  circle_child.parent_id = circles.id " +
+                ") AS childCircle " +
+                "FROM circles " +
+                "WHERE circles.id >= :id";
+
+        SqlParameterSource parameters = new MapSqlParameterSource("id", id);
+        try (Jedis jedis = pool.getResource()) {
+            String key = "circle:%d".formatted(id);
+            String raw = jedis.get(key);
+            if (raw != null){
+
+            }
+        }
+        return null;
     }
 
     private static class CircleMapper implements RowMapper<Circle> {
